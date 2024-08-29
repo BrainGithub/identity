02a86a3272a078e7b5df73de59e1723caaa5e77e
zxp <zxp@zxp.local>
2024-08-30 02:02:21 +0800

    Identity controller
    Signed-off-by: zxp <zxp@zxp.local>

---
+++ api/v1/identity_types.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// IdentitySpec defines the desired state of Identity
+type IdentitySpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Quantity of instances
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=10
+	Size int32 `json:"size"`
+
+	// Name of the ConfigMap for configuration
+	// +kubebuilder:validation:MaxLength=32
+	// +kubebuilder:validation:MinLength=1
+	ConfigMapName string `json:"configMapName"`
+
+	// Name of the Secret for configuration
+	// +kubebuilder:validation:MaxLength=32
+	// +kubebuilder:validation:MinLength=1
+	SecretName string `json:"secretName"`
+}
+
+// IdentityStatus defines the observed state of Identity
+type IdentityStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Msg string `json:"msg"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Identity is the Schema for the identities API
+type Identity struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   IdentitySpec   `json:"spec,omitempty"`
+	Status IdentityStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// IdentityList contains a list of Identity
+type IdentityList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Identity `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Identity{}, &IdentityList{})
+}
